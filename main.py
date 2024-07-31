ab925d88a5fa9ea6c425e5d9b03bba983212db08
okbear-creater <73434255+okbear-creater@users.noreply.github.com>
2024-08-01 00:36:26 +0900

    main.py

--- main.py
+++ main.py
@@ -10,28 +10,29 @@ st.title("DJ Lineup Automator :headphones: :musical_keyboard: :technologist:")
 
 st.header("정렬 시킬 Lineup")
 
-st.subheader("금")
-friday_input = st.text_area("", height=200, key="friday_input")
+col1, col2 = st.columns(2)
 
-st.divider()
+with col1:
+    st.subheader("금")
+    friday_input = st.text_area("", height=200, key="friday_input")
 
-st.subheader("토")
-saturday_input = st.text_area("", height=200, key="saturday_input")
-
-st.divider()
+with col2:
+    st.subheader("토")
+    saturday_input = st.text_area("", height=200, key="saturday_input")
 
 if st.button("Gimme the Lineup", type="primary"):
-
     st.empty()
+
+    st.divider()
     
     st.header("정렬된 Lineup")
     
-    st.subheader("금")
-    st.text_area("", value=sort_names(friday_input), height=200, key="friday_output")
-
-    st.divider()
-
-    st.subheader("토")
-    st.text_area("", value=sort_names(saturday_input), height=200, key="saturday_output")
+    col3, col4 = st.columns(2)
     
-    st.divider()
+    with col3:
+        st.subheader("금")
+        st.text_area("", value=sort_names(friday_input), height=200, key="friday_output")
+
+    with col4:
+        st.subheader("토")
+        st.text_area("", value=sort_names(saturday_input), height=200, key="saturday_output")
